8b25d516c8795b201e034baa1971891268cdf786
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-03-21 16:13:28 -0700

    closer

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,35 +1,47 @@
-// Write your "actions" router here!
-//Action Router
-
 //ACTION IMPORT
 const Action = require("./actions-model.js");
 const express = require("express");
-
 const router = express.Router();
 
+//Middleware
+async function checkID(req, res, next) {
+  if (!req.params.id) {
+    res.status(404).json({ message: "an id is required" });
+  } else {
+    const { id } = req.params;
+    try {
+      const action = await Action.get(id);
+      if (action) {
+        req.action = action;
+        next();
+      } else {
+        next({ message: `${id} is not a valid id`, status: 404 });
+      }
+    } catch (err) {
+      next({ error: err, message: err.message, status: 500 });
+    }
+  }
+}
 //ACTIONS
 // - `[GET] /api/actions`(R in CRUD-- reading)
 
 router.get("/", async (req, res) => {
   try {
-    const newAction = await Action.get();
-    res.status(200).json(newAction);
+    const act = await Action.get();
+    if (act) {
+      res.status(200).json(act);
+    } else {
+      res.status(400).json([]);
+    }
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: err, message: err.message, status: 500 });
+    res.status(500).json({ error: err, message: err.message });
   }
 });
 
 // - `[GET] /api/actions/:id`(R in CRUD-- reading)
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const newAction = await Action.get(id);
-    res.status(200).json(newAction);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err, message: err.message, status: 500 });
-  }
+router.get("/:id", checkID, (req, res) => {
+  res.status(200).json(req.action);
 });
 
 // - `[POST]  (C in CRUD -- Creating)
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,6 +5,25 @@ const express = require("express");
 const router = express.Router();
 
 //PROJECTS
+//Middleware
+async function checkId(req, res, next) {
+  if (req.params.id) {
+    try {
+      const proj = await Project.get(req.params.id);
+      if (!proj) {
+        res.status(404).json({ message: "Invalid ID, Please try again" });
+      } else {
+        req.proj = proj;
+        req.id = req.params.id;
+        next();
+      }
+    } catch (err) {
+      res.status(500).json({ message: "Error server side" });
+    }
+  } else {
+    res.status(400).json({ message: "an id is required" });
+  }
+}
 
 // - [ ] Inside `api/projects/projects-router.js` build endpoints for performing CRUD operations on _projects_:
 
@@ -15,20 +34,13 @@ router.get("/", async (req, res) => {
     res.status(200).json(newProject);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: err, message: err.message, status: 500 });
+    res.status(500).json({ error: err, message: err.message });
   }
 });
 
 // - `[GET] /api/projects/:id`(R in CRUD-- reading)
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const newProject = await Project.get(id);
-    res.status(200).json(newProject);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err, message: err.message, status: 500 });
-  }
+router.get("/:id", checkId, async (req, res) => {
+  res.status(200).json(req.proj);
 });
 
 // - `[POST] /api/projects` (C in CRUD -- Creating)
@@ -80,20 +92,18 @@ router.delete("/:id", async (req, res) => {
 // - [ ] Inside `api/projects/projects-router.js` add an endpoint for retrieving the list of Projects for a project:
 
 // - `[GET] /api/projects/:id/Projects` sends an array of Projects (or an empty array) as the body of the response. //Not sure how to actually get back only the Projects
-router.get("/:id/actions", async (req, res) => {
-  const { id } = req.params;
+router.get("/:id/actions", checkId, async (req, res) => {
   try {
-    const newProject = await Project.get(id);
-    if (newProject) {
-      res.status(200).json(newProject);
+    const projectActions = await Project.getProjectActions(req.id);
+    if (projectActions) {
+      res.status(200).json(projectActions);
     } else {
-      res.status(404).json({
-        message: "This ID doesn't exist, check what id's exist at api/projects",
-      });
+      res
+        .status(404)
+        .json({ message: "no action associated with this project" });
     }
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err, message: err.message, status: 500 });
+    res.status(500).json({ error: err, message: err.message });
   }
 });
 module.exports = router;
